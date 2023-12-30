16b2335eba8ea5988898817ceab0e8db60bb17dd
mareldev <marcosdelgadomac@gmail.com>
2023-12-30 14:18:31 +0100

    Add unicade to proyect

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,50 @@
+import { useState } from 'react'
+
+const StatisticLine = (props) => {
+  return (
+    <tr>
+      <td>{props.text}</td>
+      <td>{props.value}</td>
+    </tr>
+  )
+}
+
+const Statistics = ({ good, neutral, bad }) => {
+  let all = good + neutral + bad
+  if (all > 0) {
+    return (
+      <table>
+        <tbody>
+          <StatisticLine text="good" value={good} />
+          <StatisticLine text="neutral" value={neutral} />
+          <StatisticLine text="bad" value={bad} />
+          <StatisticLine text="all" value={good + neutral + bad} />
+          <StatisticLine text="averange" value={(good + neutral + bad) / 3} />
+          <StatisticLine text="positive" value={((good) / (good + neutral + bad) * 100) + "%"} />
+        </tbody>
+      </table>
+    )
+  }
+
+  return <p>no feedback given</p>
+}
+
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <>
+      <h1>give feedback</h1>
+      <button onClick={() => setGood(good + 1)}>good</button >
+      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <h1>statistics</h1>
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </>
+  )
+}
+
+export default App
